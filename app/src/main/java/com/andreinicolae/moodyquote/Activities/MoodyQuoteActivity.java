042d1299685b3fcfde18b9bc9dc97474ee32fb63
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-09-15 20:14:44 +0300

    Refactored main activity, updated layout

--- app/src/main/java/com/andreinicolae/moodyquote/Activities/MoodyQuoteActivity.java
+++ app/src/main/java/com/andreinicolae/moodyquote/Activities/MoodyQuoteActivity.java
@@ -1,30 +1,15 @@
 package com.andreinicolae.moodyquote.Activities;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.view.View;
 
 import com.andreinicolae.moodyquote.R;
 
-public class MoodyQuoteActivity extends Activity {
+public class MoodyQuoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moody_quote);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MoodyQuoteActivity.this, QuoteManagementActivity.class);
-                startActivity(myIntent);
-            }
-        });
     }
-
 }
